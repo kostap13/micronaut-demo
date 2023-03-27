112058410b0990618d8a654c16ffd7a24409058d
Konstantin Latypov <camoroh13@gmail.com>
2023-03-27 21:39:04 +0400

    Init

---
+++ src/main/java/com/example/micronaut/covid/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.micronaut.covid;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/demo")
+public class DemoController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
